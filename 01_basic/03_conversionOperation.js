09c514e1e20798cc8288b6f887cb71fe61adc817
Abhikrishn <abhishekkrishna467@gmail.com>
2024-06-04 01:04:08 +0530

    datatype conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score="Abhishek"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" =>33
+//"33abc" =>NaN
+//TRUE =1 ;FALSE=0
+
+let isLoggedIn="Abhishek"
+
+let ValueInBoolean=Boolean(isLoggedIn)
+console.log(typeof ValueInBoolean)
+console.log(ValueInBoolean)
+
+//1 =>true ; 0=>False
+// "" =>false
+//"Abhishek"=>true
+
+let someNumber=33
+
+let valueInString=String(someNumber)
+console.log(valueInString)
+console.log(typeof valueInString)
