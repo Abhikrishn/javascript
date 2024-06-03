1a472a8fda7a824ba632f5fa0fdc6adaf15c6a7b
Abhikrishn <abhishekkrishna467@gmail.com>
2024-06-03 21:00:34 +0530

    Datatype

---
+++ 02_Datatypes.js
@@ -0,0 +1,34 @@
+// Data Type:-use to hold different type of value
+
+"Use strict"; //treat Js code as a newer version
+
+//alert  (3+3) we are using Node js not browser
+
+
+console.log(3 +
+    3) //code redeability should be high
+
+
+
+    console.log("Abhishek")
+
+
+    let name ="Hitesh" //string
+    let age =19 //number
+    let isLoggedIn=false //boolean
+
+
+
+    //number =>2 to power 53
+    //bigint
+    //string=>""
+    //Boolean=>true/false
+    //null=> it is standloan value (empty value) ex:temp o that means temp is 0 but tempt not given so 0 is not a null
+    //undefine=> ex:let a; it occur when varible is declare but value not assign
+    //symbol=>unique
+
+    //oject
+
+    console.log(typeof "") ;
+    console.log(null) //it is object when shows output
+    console.log(undefine)//it is undefine
