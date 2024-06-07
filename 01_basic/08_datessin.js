32eb90322e11180db6e20215b6b1e847aea5e071
Abhikrishn <abhishekkrishna467@gmail.com>
2024-06-07 21:57:44 +0530

    date and time

---
+++ 01_basic/08_datessin.js
@@ -0,0 +1,20 @@
+// Dates
+
+let myDate=new Date()
+// console.log(myDate)
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+let mycreatedDate=new Date(2023,0,5)
+// console.log(mycreatedDate.toDateString())
+
+// let myTimeStamp=Date.now()
+ // console.log(myTimeStamp)
+// console.log(Math.floor(Date.now()/1000))
+
+// `${newDate.getDay() } and the time`
