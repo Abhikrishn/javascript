9c4b98895a9866157e2f37c89ecf677a3b7458ef
Abhikrishn <abhishekkrishna467@gmail.com>
2024-06-19 23:48:07 +0530

    do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,42 @@
+// while or do while loop:
+
+//while loop
+
+// sytax: while (condition) {
+    
+// let index = 0
+// while(index<=10){
+//     console.log(` the value of index is ${index}`)
+//     index =index +2
+// }
+
+// let myArray=["flash","spiderman","batman"]
+// let arr=0
+// while(arr<myArray.length){
+//     console.log(` the valu is ${myArray[arr]}`)
+//     arr =arr+1
+// }
+
+
+
+// Do while loop
+// sytax: do {
+    
+// } while (condition);
+
+// let score=0
+
+// do{
+//     console.log(`score is ${score}`)
+//     score++
+// }while(score<=10)
+
+//do while is similar to while loop with one key difference:
+//the code block is executed at least once before the condition is checked
+
+let score=11
+do{
+    //console.log(`the scor is ${score}`)
+    console.log(score)
+score++
+}while(score<=10)
